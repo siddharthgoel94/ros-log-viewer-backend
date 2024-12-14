e8b00fd62bc18b503d588c56993776eabec19bda
siddharthgoel94 <siddharth_goel2105@outlook.com>
2024-12-14 15:21:46 +0530

    configured main.py

--- main.py
+++ main.py
@@ -18,3 +18,9 @@ app.include_router(logs.router)
 @app.get("/")
 def root():
     return {"message": "Welcome to the ROS Log Viewer Backend"}
+
+if __name__ == "__main__":
+   
+    port = int(os.environ.get("PORT", 8000))
+    
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
